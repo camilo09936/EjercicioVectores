fe6c89260f8fe7b977f6a01e97c0c3a73e1ef38f
Camilo Pérez <Camilo09936@gmail.com>
2025-09-05 22:55:58 -0500

    Segunda Version

--- Main.java
+++ Main.java
@@ -11,13 +11,15 @@ public class Main {
         //--- menu de opciones -----
 
         while(!salida){// mientras no sea falso se repite el menu
-        System.out.println("Bienvenido al Sistema de Gestion de Personas");
-        System.out.println("1. Agregar Persona");
-        System.out.println("2. Mostrar Personas");
-        System.out.println("3. Eliminar Personas");
-        System.out.println("4. Buscar Personas");
-        System.out.println("5. Mostrar Capacidad y Numero De Personas");
-        System.out.println("6. Salir");
+        System.out.println(" ---------------------------------------------------------------");
+        System.out.println("| Bienvenido al Sistema de Gestion de Personas                  |");
+        System.out.println("| 1. Agregar Persona                                            |");
+        System.out.println("| 2. Mostrar Personas                                           |");
+        System.out.println("| 3. Eliminar Personas                                          |");
+        System.out.println("| 4. Buscar Personas                                            |");
+        System.out.println("| 5. Mostrar Capacidad y Numero De Personas                     |");
+        System.out.println("| 6. Salir                                                      |");
+        System.out.println(" ---------------------------------------------------------------");
         //try cath para manejar excepciones y controlar los errores en tiempo de ejecucion//
         try{
         System.out.println("Escribe una de las opciones");
@@ -46,11 +48,15 @@ public class Main {
             v.mostrarVector();
             break;
             case 3:{
-            System.out.println("Ingrese Posicion a Eliminar: ");
-            int pos= leer.nextInt();
-            leer.nextLine();
-            
-            v.eliminarPersona(pos);
+            System.out.println("Ingrese Nombre a Eliminar: ");
+            String n=leer.next();
+            int pos= v.buscarPersona(n);
+            if (pos != -1){
+                v.eliminarPersona(pos);
+                System.out.println(n + " Eliminado en la posicion " + pos);
+            }else{
+                System.out.println("No se Encontro Ninguna Persona con el Nombre: " +n);
+            }
             break;
             }
             case 4:{
